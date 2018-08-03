275bcfac9ecf621f36abc80d5587af71fd21f51d
Kevin B <biojoutk1106@gmail.com>
2018-08-03 18:10:56 +0200

    Fix for old ban entries

--- src/commands/general/baninfo.js
+++ src/commands/general/baninfo.js
@@ -23,23 +23,31 @@ class BaninfoCommand extends Command {
       else if (foundUsers.length === 1) user = foundUsers[0];
       else return context.replyWarning(this.client.finder.formatUsers(foundUsers, context.settings.misc.locale));
     } else {
-      return context.replyError(context.__('finderUtil.findUsers.noQuery'));
+      return context.replyError(context.__('baninfo.noQuery'));
     }
 
-    const logEntry = auditLogs.entries.find(e => e.action === 'MEMBER_BAN_ADD' && e.target.id === user.id) || ({});
+    const logEntry = auditLogs.entries.find(e => e.action === 'MEMBER_BAN_ADD' && e.target.id === user.id);
 
     const embed = new RichEmbed()
       .setDescription([
         `${this.dot} ${context.__('baninfo.embed.targetID')}: **${user.id}**`,
-        `${this.dot} ${context.__('baninfo.embed.executor')}: **${logEntry.executor.username}**#${logEntry.executor.discriminator} (ID:${logEntry.executor.id})`,
-        `${this.dot} ${context.__('baninfo.embed.reason')}: ${logEntry.reason ? `**${logEntry.reason}**` : context.__('global.none')}`,
-        `${this.dot} ${context.__('baninfo.embed.date')}: **${context.formatDate(logEntry.createdTimestamp)}**`,
-      ].join('\n'));
+        `${this.dot} ${context.__('baninfo.embed.executor')}: ${logEntry && logEntry.executor ? `**${logEntry.executor.username}**#${logEntry.executor.discriminator} (ID:${logEntry.executor.id})` : `*${context.__('global.unknwon')}*`}`,
+        `${this.dot} ${context.__('baninfo.embed.reason')}: ${logEntry && logEntry.reason ? `**${logEntry.reason}**` : context.__('global.none')}`,
+        `${this.dot} ${context.__('baninfo.embed.date')}: ${logEntry ? `**${context.formatDate(logEntry.createdTimestamp)}**` : context.__('global.none')}`,
+      ].join('\n'))
+      .setThumbnail(user.avatar
+        ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.${user.avatar.startsWith('a_') ? 'gif' : 'png'}`
+        : this.getDefaultAvatar(user.discriminator));
 
     context.reply(
       context.__('baninfo.title', { user: `**${user.username}**#${user.discriminator}` }),
       { embed },
     );
+  }
+
+  getDefaultAvatar(discriminator) {
+    const defaultAvatarID = discriminator % 5;
+    return `https://cdn.discordapp.com/embed/avatars/${defaultAvatarID}.png`;
   }
 }
 
