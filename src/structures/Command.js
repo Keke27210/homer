2a3ee850ba3d93a9a1ad7ee264e519dd6c859ae4
Kevin B <biojoutk1106@gmail.com>
2018-08-26 16:41:30 +0200

    Don't fetch anymore since we fetch all members at the bot start

--- src/structures/Command.js
+++ src/structures/Command.js
@@ -155,12 +155,6 @@ class Command {
       ));
     }
 
-    // We fetch members in the guild IF they have not been and IF command needs user fetching
-    if (context.message.guild && !this.client.fetchDone.includes(context.message.guild.id)) {
-      context.message.guild.fetchMembers();
-      this.client.fetchDone.push(context.message.guild.id);
-    }
-
     // Insert stats and cooldown
     if (!this.client.config.owners.includes(context.message.author.id)) {
       this.client.database.insertDocument('cooldown', { id: context.message.author.id, time: Date.now() });
--- src/structures/DiscordClient.js
+++ src/structures/DiscordClient.js
@@ -23,7 +23,6 @@ class DiscordClient extends Client {
     this.constants = Constants;
     this.events = [];
     this.unavailable = [];
-    this.fetchDone = [];
     this.currentBroadcasts = [];
     this.voiceBroadcasts = [];
     this.ready = false;
