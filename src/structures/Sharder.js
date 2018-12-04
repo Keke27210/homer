e5e198bdc3e7946e427633191727dd81b1a88837
iDroid27 <biojoutk1106@gmail.com>
2018-12-04 19:27:32 +0100

    No need of cleverbot shit anymore

--- src/structures/Sharder.js
+++ src/structures/Sharder.js
@@ -5,26 +5,10 @@ class Sharder extends ShardingManager {
   constructor(file, config) {
     super(file, config.sharder);
     this.config = config;
-
-    // Spawn cleverbot
-    this.createBot();
   }
 
   async eval(code) {
     await eval(code);
-  }
-
-  createBot() {
-    request
-      .post('https://cleverbot.io/1.0/create')
-      .set({ 'Content-Type': 'application/json' })
-      .send({
-        user: this.config.api.cleverbotUser,
-        key: this.config.api.cleverbotKey,
-        nick: this.config.discord.id,
-      })
-      .then(() => console.log('[Cleverbot] Created instance successfully!'))
-      .catch(res => console.error(`[Cleverbot] Failed to create instance! Status: ${res.body.status}`));
   }
 }
 
