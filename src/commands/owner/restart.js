7f109a250a5dfefa962dd1f6011ffba05d09fa2b
iDroid27 <biojoutk1106@gmail.com>
2018-12-29 12:15:08 +0100

    Improvements

--- src/commands/owner/restart.js
+++ src/commands/owner/restart.js
@@ -12,6 +12,11 @@ class RestartCommand extends Command {
 
   async execute(context) {
     const message = await context.replyLoading(`Restarting **${this.client.shard.count}** shards...`);
+    await this.client.database.updateDocument('bot', 'settings', { reboot: [context.message.channel.id, message.id] });
+
+    // Sending messages in radio channels
+
+
     await this.client.shard.send({ type: 'shutdown', message: `${context.message.channel.id}|${message.id}|true` });
   }
 }
--- src/events/Ready.js
+++ src/events/Ready.js
@@ -1,4 +1,3 @@
-const mtz = require('moment-timezone');
 const Event = require('../structures/Event');
 
 class ReadyEvent extends Event {
@@ -12,6 +11,18 @@ class ReadyEvent extends Event {
       type: 'log',
       message: `This shard is READY - ${this.client.guilds.size} servers - ${this.client.users.size} users`,
     });
+
+    // If it is a reboot, edit the message and clear cache
+    const reboot = this.client.database.getDocument('bot', 'settings').then(s => s.reboot);
+    if (reboot) {
+      const channel = this.client.channels.get(reboot[0]);
+      if (channel) {
+        const message = await channel.fetchMessage(reboot[1]).catch(() => null);
+        if (message) message.edit(`${this.client.constants.emotes.success} Successfully restarted all shards!`);
+      }
+
+      this.client.database.updateDocument('bot', 'settings', { reboot: null });
+    }
 
     // Update game & bot list count
     this.client.update.updateGame();
