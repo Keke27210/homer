359ce4bac7911b2d53ab451c5bce54856f192319
iDroid27 <biojoutk1106@gmail.com>
2018-12-29 17:49:39 +0100

    Fixed it

--- src/managers/RadioManager.js
+++ src/managers/RadioManager.js
@@ -45,7 +45,10 @@ class RadioManager extends Manager {
   stopBroadcast(broadcast, error, play = true) {
     broadcast.destroy();
     this.broadcasts.splice(this.broadcasts.findIndex(b => b.radio === broadcast.radio), 1);
-    broadcast.dispatchers.forEach(d => d.player.voiceConnection.playFile(this.ERROR_PATH, { volume: 1, bitrate: 64 }));
+    broadcast.dispatchers.forEach((dispatcher) => {
+      dispatcher.end();
+      dispatcher.player.voiceConnection.playStream(this.ERROR_PATH, { volume: 1, bitrate: 64 });
+    });
     this.client.print(`RADIO: Voice broadcast error for ${broadcast.radio}`);
   }
 
