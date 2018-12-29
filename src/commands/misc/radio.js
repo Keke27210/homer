126e6875620c506993fa337a296956c2c99f1611
iDroid27 <biojoutk1106@gmail.com>
2018-12-29 11:36:23 +0100

    Since it only accepts a function...

--- src/commands/misc/radio.js
+++ src/commands/misc/radio.js
@@ -79,7 +79,7 @@ class TuneSubcommand extends Command {
 
     const dispatcher = await connection.playBroadcast(broadcast, { volume: context.settings.radio.volume || 0.5 });
     dispatcher.on('error', error => this.client.radio.dispatcherError(context, dispatcher, error));
-    dispatcher.once('speaking', message.edit(context.__('radio.tune.playing', { name: broadcast.name })));
+    dispatcher.once('speaking', () => message.edit(context.__('radio.tune.playing', { name: broadcast.name })));
   }
 }
 
