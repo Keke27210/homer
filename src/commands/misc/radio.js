3cdad75608c95708d097854a2e1ee24c1ce1fa73
iDroid27 <biojoutk1106@gmail.com>
2018-12-29 11:27:54 +0100

    lol yea that's dumb

--- src/commands/misc/radio.js
+++ src/commands/misc/radio.js
@@ -129,7 +129,7 @@ class StopSubcommand extends Command {
     if (!channel.members.has(context.message.author.id)) return context.replyWarning(context.__('radio.notInChannel', { name: channel.name }));
 
     const connection = this.client.voiceConnections.get(context.message.guild.id);
-    if (!connection) return context.replyWarning(context.__('radio.stop.noActiveStream', { name: connection.channel.name }));
+    if (!connection) return context.replyWarning(context.__('radio.stop.noActiveStream', { name: channel.name }));
     await channel.leave();
 
     context.replySuccess(context.__('radio.stop.done'));
