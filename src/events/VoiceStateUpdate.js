d4adea83974e19e38f28790f29e9c6eb5b7aa9eb
iDroid27 <biojoutk1106@gmail.com>
2018-12-29 10:31:22 +0100

    Forgot to make the function asynchronous...

--- src/events/VoiceStateUpdate.js
+++ src/events/VoiceStateUpdate.js
@@ -5,12 +5,11 @@ class VoiceStateUpdate extends Event {
     super(client, 'voiceStateUpdate');
   }
 
-  handle(oldMember, newMember) {
+  async handle(oldMember, newMember) {
     const settings = await this.client.database.getDocument('settings', newMember.guild.id);
     if (!settings) return;
 
     if (newMember.id === this.client.user.id) {
-      console.log(`Current: ${newMember.voiceChannel.id} - Radio: ${settings.radio.channel}`)
       if (newMember.voiceChannel && (newMember.voiceChannel.id !== settings.radio.channel)) {
         await newMember.voiceChannel.leave();
         delete this.client.currentBroadcasts[context.message.guild.id];
