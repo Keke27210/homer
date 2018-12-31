368c4c183c614cad8d7609e8f84975992a2422e9
iDroid27 <biojoutk1106@gmail.com>
2018-12-31 14:02:33 +0100

    "tune" as an aliase for "radio tune"

---
+++ src/commands/misc/tune.js
@@ -0,0 +1,17 @@
+const Command = require('../../structures/Command');
+
+class TuneCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'tune',
+      category: 'misc',
+    });
+  }
+
+  async execute(context) {
+    const tuneCommand = this.client.commands.getCommand('radio').children.find(c => c.name === 'tune');
+    tuneCommand.execute(context);
+  }
+}
+
+module.exports = TuneCommand;
