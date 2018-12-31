7b2023c5af10ba1d6dde26fbb8b53cf9615c1b87
iDroid27 <biojoutk1106@gmail.com>
2018-12-31 22:05:52 +0100

    Better way to deal with it

--- src/managers/RadioManager.js
+++ src/managers/RadioManager.js
@@ -34,14 +34,14 @@ class RadioManager extends Manager {
       this.clearBroadcast(broadcast.radio);
 
       // Update radio statistics
-      const stats = await this.client.database.getDocument('radioStats', broadcast.radio);
+      const stats = await this.client.database.getDocument('radioStats', broadcast.radio) || ({ id: broadcast.radio, entries: [] });
       const index = stats.entries.findIndex(e => e.id === dispatcher.player.voiceConnection.channel.guild.id);
       stats.entries.push({
         id: dispatcher.player.voiceConnection.channel.guild.id,
         time: (stats.entries[index] ? stats.entries[index].time : 0) + this.stats[dispatcher.player.voiceConnection.channel.guild.id],
       });
       if (index !== -1) stats.entries.splice(index, 1);
-      this.client.database.updateDocument('radioStats', broadcast.radio, stats);
+      this.client.database.insertDocument('radioStats', stats, { conflict: 'update' });
       delete this.stats[dispatcher.player.voiceConnection.channel.guild.id];
     });
 
