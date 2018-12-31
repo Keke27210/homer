4a61db293d48fd2cc34e82d2ff4c87ea87e86a7d
iDroid27 <biojoutk1106@gmail.com>
2018-12-31 12:48:08 +0100

    Fixed prefix time on logging files

--- src/util/LoggerUtil.js
+++ src/util/LoggerUtil.js
@@ -8,25 +8,30 @@ class LoggerUtil extends Util {
     this.LOG_PATH = './logs';
   }
 
+  prefixTime() {
+    const date = new Date();
+    return `[${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}.${date.getUTCMilliseconds()}]`;
+  }
+
   generateFilename(type) {
     const date = moment().format('DD-MM-YYYY');
     return `${type}/${this.client.shard.id}_${date}`;
   }
 
   info(message) {
-    appendFile(`${this.LOG_PATH}/${this.generateFilename('info')}`, `[${new Date()}] ${message}\r\n\r\n`, (err) => {
+    appendFile(`${this.LOG_PATH}/${this.generateFilename('info')}`, `[${prefixTime()}] ${message}\r\n\r\n`, (err) => {
       if (err) console.error(err);
     });
   }
 
   warn(message) {
-    appendFile(`${this.LOG_PATH}/${this.generateFilename('warn')}`, `[${new Date()}] ${message}\r\n\r\n`, (err) => {
+    appendFile(`${this.LOG_PATH}/${this.generateFilename('warn')}`, `[${prefixTime()}] ${message}\r\n\r\n`, (err) => {
       if (err) console.error(err);
     });
   }
 
   error(message) {
-    appendFile(`${this.LOG_PATH}/${this.generateFilename('error')}`, `[${new Date()}] ${message}\r\n\r\n`, (err) => {
+    appendFile(`${this.LOG_PATH}/${this.generateFilename('error')}`, `[${prefixTime()}] ${message}\r\n\r\n`, (err) => {
       if (err) console.error(err);
     });
   }
