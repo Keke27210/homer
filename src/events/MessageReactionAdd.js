82e773911091e9910f6efab0cac087d03b3a388a
iDroid27 <biojoutk1106@gmail.com>
2019-01-01 12:40:40 +0100

    Fixed yet another bug

--- src/events/MessageReactionAdd.js
+++ src/events/MessageReactionAdd.js
@@ -13,7 +13,7 @@ class MessageReactionAddEvent extends Event {
       // Trying to delete the reaction
       const message = reaction.message;
       if (!message.guild || !message.channel.permissionsFor(this.client.user).has('MANAGE_MESSAGES')) return;
-      reaction.remove(user);
+      if (user.id !== this.client.user.id) reaction.remove(user);
     }
   }
 }
