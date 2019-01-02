e5753a6ed9c987a58fe6e3ba2e9796b25d56616d
iDroid27 <biojoutk1106@gmail.com>
2019-01-02 15:15:57 +0100

    Fixed some stuff

--- src/commands/bot/donators.js
+++ src/commands/bot/donators.js
@@ -26,17 +26,11 @@ class DonatorsCommand extends Command {
       .join('\n');
 
     const embed = new RichEmbed()
-      .setDescription(context.__(
-        'donators.text',
-        { link: this.client.constants.donationLink },
-      ))
-      .addField(context.__('donators.donators'), donators || context.__('global.none'))
-      .addField(context.__('donators.perks'), perks || context.__('global.none'));
+      .setDescription(context.__('donators.text', { link: this.client.constants.donationLink }))
+      .addField(context.__('donators.donators'), donators || context.__('global.none'), true)
+      .addField(context.__('donators.perks'), perks || context.__('global.none'), true);
 
-    context.reply(
-      context.__('donators.main', { name: this.client.user.username }),
-      { embed },
-    );
+    context.reply(context.__('donators.main', { name: this.client.user.username }), { embed });
   }
 }
 
