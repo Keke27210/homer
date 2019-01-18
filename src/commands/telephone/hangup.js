e9b86c7f1152958099122d389b0c640e222158a4
iDroid27 <biojoutk1106@gmail.com>
2019-01-18 21:16:38 +0100

    Hangup command ready for group calls

--- src/commands/telephone/hangup.js
+++ src/commands/telephone/hangup.js
@@ -10,26 +10,29 @@ class HangupCommand extends Command {
   }
 
   async execute(context) {
-    const callObject = await this.client.database.getDocuments('calls', true)
-      .then(calls => calls.find(c => [c.sender.id, c.receiver.id].includes(context.message.channel.id)));
-    if (!callObject) return context.replyError(context.__('telephone.noCall'));
-    const state = (context.message.channel.id === callObject.sender.id) ? 'sender' : 'receiver';
+    const calls = await this.client.database.getDocuments('calls', true);
+    if (!calls) return context.replyError(context.__('telephone.noCall'));
 
-    this.client.database.deleteDocument('calls', callObject.id);
-    if (callObject.state === 1) {
-      if (state === 'sender') {
-        this.client.sendMessage(callObject.sender.id, this.client.__(callObject.sender.locale, 'hangup.author'));
-        this.client.sendMessage(callObject.receiver.id, this.client.__(callObject.receiver.locale, 'hangup.target'));
+    const call = calls.find(c => c.type === 0 ? [c.sender.id, c.receiver.id].includes(context.message.channel.id) : c.receivers.find(r => r.id === context.message.channel.id));
+    await this.client.database.deleteDocument('calls', call.id);    
+
+    if (call.type === 0) {
+      const state = (context.message.channel.id === call.sender.id) ? 'sender' : 'receiver';
+      if (call.state === 1) {
+        if (state === 'sender') {
+          this.client.sendMessage(call.sender.id, this.client.__(call.sender.locale, 'hangup.author'));
+          this.client.sendMessage(call.receiver.id, this.client.__(call.receiver.locale, 'hangup.target'));
+        } else {
+          this.client.sendMessage(call.sender.id, this.client.__(call.sender.locale, 'hangup.target'));
+          this.client.sendMessage(call.receiver.id, this.client.__(call.receiver.locale, 'hangup.author'));
+        }
+      } else if (state === 'sender') {
+        this.client.sendMessage(call.sender.id, this.client.__(call.sender.locale, 'hangup.author'));
+        this.client.updateMessage(call.receiver.id, call.receiver.message, this.client.__(call.receiver.locale, 'call.receiverMissed', { number: callObject.sender.number }));
       } else {
-        this.client.sendMessage(callObject.sender.id, this.client.__(callObject.sender.locale, 'hangup.target'));
-        this.client.sendMessage(callObject.receiver.id, this.client.__(callObject.receiver.locale, 'hangup.author'));
+        this.client.sendMessage(call.sender.id, this.client.__(call.sender.locale, 'hangup.target'));
+        this.client.sendMessage(call.receiver.id, this.client.__(call.receiver.locale, 'hangup.author'));
       }
-    } else if (state === 'sender') {
-      this.client.sendMessage(callObject.sender.id, this.client.__(callObject.sender.locale, 'hangup.author'));
-      this.client.updateMessage(callObject.receiver.id, callObject.receiverMessage, this.client.__(callObject.receiver.locale, 'call.receiverMissed', { number: callObject.sender.number }));
-    } else {
-      this.client.sendMessage(callObject.sender.id, this.client.__(callObject.sender.locale, 'hangup.target'));
-      this.client.sendMessage(callObject.receiver.id, this.client.__(callObject.receiver.locale, 'hangup.author'));
     }
   }
 }
