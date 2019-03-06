7f6928ce8dde10cb3d778f9dc8babe4bb5e15794
iDroid27 <biojoutk1106@gmail.com>
2019-03-06 21:22:26 +0100

    Fixed method name

--- src/commands/misc/shazam.js
+++ src/commands/misc/shazam.js
@@ -36,7 +36,7 @@ class ShazamCommand extends Command {
     this.client.shazamWork.push(context.message.guild.id);
     const m = await context.replyLoading(context.__('shazam.recording', { user: `**${user.username}**#${user.discriminator}` }));
 
-    this.client.other.recordMusic(context.message.guild.voiceConnection, user)
+    this.client.other.recordSound(context.message.guild.voiceConnection, user)
       .then(async (data) => {
         if (!data) return m.edit(`${this.client.constants.emotes.error} ${context.__('shazam.error', { status: '500' })}`);
         await m.edit(`${this.client.constants.emotes.loading} ${context.__('shazam.sending')}`);
