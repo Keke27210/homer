aa51920283a9f6df6f3dabf33bdb413a6f0d0715
Kevin <biojoutk1106@gmail.com>
2019-04-08 17:00:11 +0200

    Fixed unassigned number message

--- src/commands/telephone/text.js
+++ src/commands/telephone/text.js
@@ -26,7 +26,7 @@ class TextCommand extends Command {
 
     const status = await this.client.database.getDocument('bot', 'settings').then(s => s.telephone);
     const toSend = await this.client.database.findDocuments('telephone', { number }).then(a => a[0]);
-    if (!toSend) return context.replyWarning(context.__('telephone.notAssigned', { number }));
+    if (!toSend) return context.replyWarning(context.__('telephone.unassignedNumber', { number }));
     if (toSend.blacklist.find(b => b.channel === context.message.channel.id) || !status || !toSend.textable) {
       return context.replyWarning(context.__('text.cannotSend'));
     }
