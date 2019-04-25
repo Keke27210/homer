ea304d3cdd36ebe972a15c84cc59d7b353f0bc0c
iDroid27 <biojoutk1106@gmail.com>
2019-04-25 18:52:50 +0200

    Process anti-ghost ping before calls

--- src/events/MessageDelete.js
+++ src/events/MessageDelete.js
@@ -7,6 +7,30 @@ class MessageDeleteEvent extends Event {
   }
 
   async handle(message) {
+    // Anti ghost-ping
+    if (message.guild && message.mentions.members.size > 0) {
+      const embed = new RichEmbed()
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setDescription(message.content)
+        .setColor(message.member ? message.member.displayHexColor : undefined)
+        .setFooter(`#${message.channel.name} (${message.guild.name})`)
+        .setTimestamp(message.editedAt || message.createdAt);
+
+      message.mentions.members
+        .forEach(async (u) => {
+          if (message.author.id === u.id) return;
+          if ((Date.now() - message.createdTimestamp) > (this.client.other.isDonator(u.id) ? 3600000 : 60000)) return;
+
+          const settings = await this.client.database.getDocument('settings', u.id);
+          if (!settings || !settings.misc.antighost) return;
+
+          u.send(
+            this.client.__(settings.misc.locale, 'antighost.alert', { name: `**${message.author.username}**#${message.author.discriminator}`}),
+            { embed },
+          );
+        });
+    }
+
     // Phone
     if (message.author.id !== this.client.user.id) {
       const calls = await this.client.database.getDocuments('calls', true);
@@ -46,30 +70,6 @@ class MessageDeleteEvent extends Event {
           this.client.deleteMessage(receiver.id, targetMessage.id);
         }
       }
-    }
-
-    // Anti ghost-ping
-    if (message.guild && message.mentions.members.size > 0) {
-      const embed = new RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
-        .setDescription(message.content)
-        .setColor(message.member ? message.member.displayHexColor : undefined)
-        .setFooter(`#${message.channel.name} (${message.guild.name})`)
-        .setTimestamp(message.editedAt || message.createdAt);
-
-      message.mentions.members
-        .forEach(async (u) => {
-          if (message.author.id === u.id) return;
-          if ((Date.now() - message.createdTimestamp) > (this.client.other.isDonator(u.id) ? 3600000 : 60000)) return;
-
-          const settings = await this.client.database.getDocument('settings', u.id);
-          if (!settings || !settings.misc.antighost) return;
-
-          u.send(
-            this.client.__(settings.misc.locale, 'antighost.alert', { name: `**${message.author.username}**#${message.author.discriminator}`}),
-            { embed },
-          );
-        });
     }
   }
 }
