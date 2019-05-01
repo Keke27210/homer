cef9783743f48fdc37c6235053a26153d0b5e322
iDroid27 <biojoutk1106@gmail.com>
2019-05-01 11:14:09 +0200

    Loading splash & more fixes on strings

--- src/commands/porn/pornhub.js
+++ src/commands/porn/pornhub.js
@@ -22,14 +22,15 @@ class PornhubCommand extends Command {
     if (!search) return context.replyError(context.__('porn.noSearch'));
     if (search.length > 128) return context.replyError(context.__('porn.searchTooLong'));
 
+    const message = await context.replyLoading(context.__('global.loading'));
     const data = await request
       .get(`http://www.pornhub.com/webmasters/search?query=${encodeURIComponent(search)}`)
       .then(r => r.body.videos)
       .catch(() => null);
-    if (!data) return context.replyWarning(context.__('porn.fetchError'));
-    if (data.length === 0) return context.replyWarning(context.__('porn.noResult', { search }));
+    if (!data) return message.edit(`${this.client.constants.emotes.warning} ${context.__('porn.fetchError')}`);
+    if (data.length === 0) return message.edit(`${this.client.constants.emotes.warning} ${context.__('porn.noResult', { search })}`);
 
-    context.reply(context.__('pornhub.title', {
+    message.edit(context.__('pornhub.title', {
       emote: this.client.constants.emotes.pornhub,
       title: data[0].title,
       url: data[0].url,
