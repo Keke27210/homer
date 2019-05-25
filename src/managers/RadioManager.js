f39b9329ff11e13ec35bdf0477623dd835137331
iDroid27 <biojoutk1106@gmail.com>
2019-05-25 15:12:48 +0200

    Standard to 64 and premium to 128kbps

--- src/managers/RadioManager.js
+++ src/managers/RadioManager.js
@@ -48,7 +48,7 @@ class RadioManager extends Manager {
     broadcast.name = radio.name;
     broadcast.radio = radio.id;
     const url = await parseURL(radio.url);
-    broadcast.playStream(url, { bitrate: 96 });
+    broadcast.playStream(url, { bitrate: 128 });
     broadcast.started = true;
     this.broadcasts.push(broadcast);
     return broadcast;
