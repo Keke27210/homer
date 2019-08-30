01fb102212a9dab36a5eff8a832a55716b59921b
iDroid27 <biojoutk1106@gmail.com>
2019-08-30 14:43:00 +0200

    Debug

--- src/commands/telephone/call.js
+++ src/commands/telephone/call.js
@@ -23,6 +23,7 @@ class CallCommand extends Command {
     if (currentStatus !== 0) return context.replyWarning(context.__('call.inCall'));
 
     const numbers = context.args.map(a => a.toUpperCase());
+    console.log(numbers)
     if (numbers.length === 0) return context.replyError(context.__('call.noNumber'));
     if (numbers.find(n => n === subscription.number)) return context.replyError(context.__('call.self'));
 
