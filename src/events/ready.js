72258549c342dd42b47a40badd959a8d8bfc1b6e
Kevin BIOJOUT <biojoutk1106@gmail.com>
2020-04-06 17:48:46 +0200

    ffmpeg: add package / providers: call createTable on ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -6,6 +6,13 @@ class ReadyEvent extends Event {
   }
 
   async handle() {
+    // Creating provider tables
+    this.client.logger.log('[database] Creating table if not exists');
+    await this.client.settings.createTable();
+    await this.client.telephone.contracts.createTable();
+    await this.client.telephone.calls.createTable();
+    await this.client.telephone.phonebook.createTable();
+
     this.client.logger.log(`[ready] Bot initialized successfully - Serving as ${this.client.user.tag}`);
   }
 }
