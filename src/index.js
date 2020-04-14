4358fea8fb44021cd6da771fa6cf9c833f4ced30
Kevin BIOJOUT <biojoutk1106@gmail.com>
2020-04-14 19:05:25 +0200

    shard: check if message is a string to handle customized messages

--- src/index.js
+++ src/index.js
@@ -29,28 +29,30 @@ sharder.logger = new Logger(-1);
  * @param {*} message Message the shard sent
  */
 function handleMessage(shard, message) {
-  if (message.startsWith('RESTART_')) {
-    const id = parseInt(message.split('_')[1]);
-    const s = sharder.shards.get(id);
-    if (s) {
-      sharder.logger.warn(`[shard ${shard.id}] RESTARTING SHARD ${id}`);
-      s.respawn(1500);
+  if (typeof message === 'string') {
+    if (message.startsWith('RESTART_')) {
+      const id = parseInt(message.split('_')[1]);
+      const s = sharder.shards.get(id);
+      if (s) {
+        sharder.logger.warn(`[shard ${shard.id}] RESTARTING SHARD ${id}`);
+        s.respawn(1500);
+      }
+    } else if (message === 'RESTARTALL') {
+      sharder.logger.warn(`[shard ${shard.id}] RESTARTING ALL SHARDS`);
+      sharder.respawnAll(5000, 1500);
+    } else if (message.startsWith('KILL_')) {
+      const id = parseInt(message.split('_')[1]);
+      const s = sharder.shards.get(id);
+      if (s) {
+        sharder.logger.warn(`[shard ${shard.id}] KILLING SHARD ${id}`);
+        s.kill();
+      }
+    } else if (message === 'KILLALL') {
+      sharder.logger.warn(`[shard ${shard.id}] KILLING ALL SHARDS`);
+      sharder.shards.forEach((s) => s.kill());
+    } else {
+      sharder.logger.log(`[shard ${shard.id}] ${message}`);
     }
-  } else if (message === 'RESTARTALL') {
-    sharder.logger.warn(`[shard ${shard.id}] RESTARTING ALL SHARDS`);
-    sharder.respawnAll(5000, 1500);
-  } else if (message.startsWith('KILL_')) {
-    const id = parseInt(message.split('_')[1]);
-    const s = sharder.shards.get(id);
-    if (s) {
-      sharder.logger.warn(`[shard ${shard.id}] KILLING SHARD ${id}`);
-      s.kill();
-    }
-  } else if (message === 'KILLALL') {
-    sharder.logger.warn(`[shard ${shard.id}] KILLING ALL SHARDS`);
-    sharder.shards.forEach((s) => s.kill());
-  } else {
-    sharder.logger.log(`[shard ${shard.id}] ${message}`);
   }
 }
 
@@ -62,5 +64,5 @@ sharder.on('shardCreate', (shard) => {
 });
 
 sharder.spawn()
-  .then(() => sharder.logger.log('[sharder] Spawning shards...'))
+  .then(() => sharder.logger.log('[sharder] Spawned shards successfully'))
   .catch(sharder.logger.error);
