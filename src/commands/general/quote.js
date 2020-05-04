da47bd58f1350dcaa7972683c2d48856671e3937
Kevin BIOJOUT <biojoutk1106@gmail.com>
2020-05-04 11:31:27 +0200

    quote: support for crossposted messages

--- src/commands/general/quote.js
+++ src/commands/general/quote.js
@@ -33,11 +33,11 @@ class QuoteCommand extends Command {
 
     const embed = new MessageEmbed()
       .setDescription(msg.content)
-      .setFooter(message._(`quote.${msg.editedTimestamp ? 'edit' : 'creation'}`, msg.id))
+      .setFooter(message._(`quote.${msg.editedTimestamp ? 'edit' : 'creation'}`, msg.id), msg.author.displayAvatarURL())
       .setTimestamp(new Date(msg.editedTimestamp || msg.createdTimestamp))
-      .setColor(msg.member.displayHexColor);
+      .setColor(msg.flags.serialize().IS_CROSSPOST ? 0x7289DA : msg.member.displayHexColor);
 
-    message.send(message._('quote.title', msg.author.tag, channel.name), embed);
+    message.send(message._('quote.title', (msg.flags.serialize().IS_CROSSPOST ? msg.author.username : msg.author.tag), channel.name), embed);
     return 0;
   }
 }
